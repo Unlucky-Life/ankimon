5e2ea4bcf25034d9c33d8a48fbb20cb7ddcf239c
anon <anon@users.noreply.github.com>
2024-03-15 23:20:03 +0100

    Update __init__.py

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -1665,8 +1665,8 @@ def evolve_pokemon(pkmn_name):
                             hp = calculate_hp(hp_stat, level, ev, iv)
                             pokemon["current_hp"] = int(hp)
                             #pokemon["gender"] = pick_random_gender(evoName.lower()) dont replace gender
-                            pokemon["growth_rate"] = search_pokedex_by_id(evoId,"growth_rate")
-                            pokemon["base_experience"] = search_pokedex_by_id(evoId,"growth_rate")
+                            pokemon["growth_rate"] = search_pokeapi_db_by_id(evoId,"growth_rate")
+                            pokemon["base_experience"] = search_pokeapi_db_by_id(evoId,"growth_rate")
                             #pokemon["growth_rate"] = search_pokeapi_db(evoName.lower(), "growth_rate")
                             #pokemon["base_experience"] = search_pokeapi_db(evoName.lower(), "base_experience")
                             abilities = search_pokedex(evoName.lower(), "abilities")
