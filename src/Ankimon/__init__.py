b6a4f8a23017db3ef577ed776f003ff79e3db07c
Unlucky <77027147+Unlucky-Life@users.noreply.github.com>
2024-04-25 20:45:09 +0200

    __init__.py

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -113,6 +113,9 @@ badges_list_path = addon_dir / "addon_files" / "badges.json"
 items_list_path = addon_dir / "addon_files" / "items.json"
 rate_path = addon_dir / "user_files" / "rate_this.json"
 
+with open('items.json', 'r') as file:
+    items_list = json.load(file)
+
 #effect sounds paths
 hurt_normal_sound_path = addon_dir / "addon_sprites" / "sounds" / "HurtNormal.mp3"
 hurt_noteff_sound_path = addon_dir / "addon_sprites" / "sounds" / "HurtNotEffective.mp3"
