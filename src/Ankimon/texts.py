19093f4a11abb7a3715ff1f1a27f2daf06d720ef
helpau <helpau@test.com>
2024-08-04 22:08:42 +0500

    terms_text

--- src/Ankimon/texts.py
+++ src/Ankimon/texts.py
@@ -185,3 +185,14 @@ remember_attack_details_window_template_end = """
     </body>
     </html>
     """
+
+terms_text = """§1 Disclaimer of Liability
+(1) The user acknowledges that the use of the downloaded files is at their own risk. \n The provider assumes no liability for any damages, direct or indirect,\n that may arise from the download or use of such files.
+(2) The provider is not responsible for the content of the downloaded files or \n for the legal consequences that may result from the use of the files. \n Each user is obligated to inform themselves about the legality of the use \n before using the files and to use the files only in a manner that does not cause any legal violations.
+
+§2 Copyright Infringements
+(1) The user agrees to respect copyright and other protective rights of third parties. \n It is prohibited for the user to download, reproduce, distribute, or make publicly available any copyrighted works \n without the required permission of the rights holder.
+(2) In the event of a violation of copyright provisions, the user bears full responsibility and the resulting consequences. \n The provider reserves the right to take appropriate legal action \n in the event of becoming aware of any rights violations and to block access to the services.
+                       
+Check out https://pokeapi.co/docs/v2#fairuse and https://github.com/smogon/pokemon-showdown for more information.
+                       """
