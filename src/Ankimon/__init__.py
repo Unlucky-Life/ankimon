9dce1e2754d5e177d543721e985e027b5163e23b
helpau <helpau@test.com>
2024-08-09 23:22:16 +0500

    business.py-special_pokemon_names_for_min_level

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -56,6 +56,7 @@ from .texts import _bottomHTML_template, button_style, pokedex_html_template, \
                     thankyou_message_text, dont_show_this_button_text
 
 from .const import *
+from .business import *
 
 #from .download_pokeapi_db import create_pokeapidb
 config = mw.addonManager.getConfig(__name__)
@@ -664,58 +665,6 @@ def check_id_ok(id_num):
 # if index = 40 - 100 => normal ; multiply with damage
 # if index < 40 => attack misses
 
-def special_pokemon_names_for_min_level(name):
-    if name == "flabébé":
-        return "flabebe"
-    elif name == "sirfetch'd":
-        return "sirfetchd"
-    elif name == "farfetch'd":
-        return "farfetchd"
-    elif name == "porygon-z":
-        return "porygonz"
-    elif name == "kommo-o":
-        return "kommoo"
-    elif name == "hakamo-o":
-        return "hakamoo"
-    elif name == "jangmo-o":
-        return "jangmoo"
-    elif name == "mr. rime":
-        return "mrrime"
-    elif name == "mr. mime":
-        return "mrmime"
-    elif name == "mime jr.":
-        return "mimejr"
-    elif name == "nidoran♂":
-        return "nidoranm"
-    elif name == "nidoran":
-        return "nidoranf"
-    elif name == "keldeo[e]":
-        return "keldeo"
-    elif name == "mew[e]":
-        return "mew"
-    elif name == "deoxys[e]":
-        return "deoxys"
-    elif name == "jirachi[e]":
-        return "jirachi"
-    elif name == "arceus[e]":
-        return "arceus"
-    elif name == "shaymin[e]":
-        return "shaymin-land"
-    elif name == "darkrai [e]":
-        return "darkrai"
-    elif name == "manaphy[e]":
-        return "manaphy"
-    elif name == "phione[e]":
-        return "phione"
-    elif name == "celebi[e]":
-        return "celebi"
-    elif name == "magearna[e]":
-        return "magearna"
-    elif name == "type: null":
-        return "typenull"
-    else:
-        #showWarning("Error in Handling Pokémon name")
-        return name
 
 def special_pokemon_names_for_pokedex_to_poke_api_db(name):
     global pokedex_to_poke_api_db
