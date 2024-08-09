97ec99f1a6b7d728a1cdd236954558256d0a7370
helpau <helpau@test.com>
2024-08-09 23:52:34 +0500

    get_multiplier_stats,get_multiplier_acc_eva,bP_none_moves

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -2523,35 +2523,6 @@ def create_status_html(status_name):
 
     return html
 
-def get_multiplier_stats(stage):
-    # Define the mapping of stage to factor
-    stage_to_factor = {
-        -6: 3/9, -5: 3/8, -4: 3/7, -3: 3/6, -2: 3/5, -1: 3/4,
-        0: 3/3,
-        1: 4/3, 2: 5/3, 3: 6/3, 4: 7/3, 5: 8/3, 6: 9/3
-    }
-
-    # Return the corresponding factor or a default value if the stage is out of range
-    return stage_to_factor.get(stage, "Invalid stage")
-
-def get_multiplier_acc_eva(stage):
-    # Define the mapping of stage to factor
-    stage_to_factor_new = {
-        -6: 2/8, -5: 2/7, -4: 2/6, -3: 2/5, -2: 2/4, -1: 2/3,
-        0: 2/2,
-        1: 3/2, 2: 4/2, 3: 5/2, 4: 6/2, 5: 7/2, 6: 8/2
-    }
-
-    # Return the corresponding factor or a default value if the stage is out of range
-    return stage_to_factor_new.get(stage, "Invalid stage")
-
-def bP_none_moves(move):
-    target =  move.get("target", None)
-    if target == "normal":
-        damage = move.get("damage")
-        if damage is None:
-            damage = 5
-        return damage
 
 
 def effect_status_moves(move_name, mainpokemon_stats, stats, msg, name, mainpokemon_name):
