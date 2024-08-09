f0c52f78fcbdd4807aa81aaa986d864a7e751bac
helpau <helpau@test.com>
2024-08-09 23:54:40 +0500

    type_colors

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -3643,29 +3643,6 @@ def remember_attack(id, attacks, new_attack):
         else:
             showInfo("Please Select this Pokemon first as Main Pokemon ! \n Only Mainpokemons can re-learn attacks!")
     
-def type_colors(type):
-    type_colors = {
-        "Normal": "#A8A77A",
-        "Fire": "#EE8130",
-        "Water": "#6390F0",
-        "Electric": "#F7D02C",
-        "Grass": "#7AC74C",
-        "Ice": "#96D9D6",
-        "Fighting": "#C22E28",
-        "Poison": "#A33EA1",
-        "Ground": "#E2BF65",
-        "Flying": "#A98FF3",
-        "Psychic": "#F95587",
-        "Bug": "#A6B91A",
-        "Rock": "#B6A136",
-        "Ghost": "#735797",
-        "Dragon": "#6F35FC",
-        "Dark": "#705746",
-        "Steel": "#B7B7CE",
-        "Fairy": "#D685AD"
-    }
-
-    return type_colors.get(type, "Unknown")
 
 def type_icon_path(type):
     global addon_dir
--- src/Ankimon/business.py
+++ src/Ankimon/business.py
@@ -149,3 +149,27 @@ def bP_none_moves(move):
         if damage is None:
             damage = 5
         return damage
+    
+def type_colors(type):
+    type_colors = {
+        "Normal": "#A8A77A",
+        "Fire": "#EE8130",
+        "Water": "#6390F0",
+        "Electric": "#F7D02C",
+        "Grass": "#7AC74C",
+        "Ice": "#96D9D6",
+        "Fighting": "#C22E28",
+        "Poison": "#A33EA1",
+        "Ground": "#E2BF65",
+        "Flying": "#A98FF3",
+        "Psychic": "#F95587",
+        "Bug": "#A6B91A",
+        "Rock": "#B6A136",
+        "Ghost": "#735797",
+        "Dragon": "#6F35FC",
+        "Dark": "#705746",
+        "Steel": "#B7B7CE",
+        "Fairy": "#D685AD"
+    }
+
+    return type_colors.get(type, "Unknown")
