508be6cd2d6f56f705e9df74300b230c506ba37a
helpau <helpau@test.com>
2024-08-16 20:23:16 +0500

    paths

---
+++ src/Ankimon/resources.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+addon_dir = Path(__file__).parents[0]
+
+#safe route for updates
+user_path = addon_dir / "user_files"
+user_path_data = addon_dir / "user_files" / "data_files"
+user_path_sprites = addon_dir / "user_files" / "sprites"
+
+# Assign Pokemon Image folder directory name
+pkmnimgfolder = addon_dir / "user_files" / "sprites"
+backdefault = addon_dir / "user_files" / "sprites" / "back_default"
+frontdefault = addon_dir / "user_files" / "sprites" / "front_default"
+#Assign saved Pokemon Directory
+mypokemon_path = addon_dir / "user_files" / "mypokemon.json"
+mainpokemon_path = addon_dir / "user_files" / "mainpokemon.json"
+battlescene_path = addon_dir / "addon_sprites" / "battle_scenes"
+battlescene_path_without_dialog = addon_dir / "addon_sprites" / "battle_scenes_without_dialog"
+battle_ui_path = addon_dir / "pkmnbattlescene - UI_transp"
+type_style_file = addon_dir / "addon_files" / "types.json"
+next_lvl_file_path = addon_dir / "addon_files" / "ExpPokemonAddon.csv"
+berries_path = addon_dir / "user_files" / "sprites" / "berries"
+background_dialog_image_path  = addon_dir / "background_dialog_image.png"
+pokedex_image_path = addon_dir / "addon_sprites" / "pokedex_template.jpg"
+evolve_image_path = addon_dir / "addon_sprites" / "evo_temp.jpg"
+learnset_path = addon_dir / "user_files" / "data_files" / "learnsets.json"
+pokedex_path = addon_dir / "user_files" / "data_files" / "pokedex.json"
+moves_file_path = addon_dir / "user_files" / "data_files" / "moves.json"
+items_path = addon_dir / "user_files" / "sprites" / "items"
+badges_path = addon_dir / "user_files" / "sprites" / "badges"
+itembag_path = addon_dir / "user_files" / "items.json"
+badgebag_path = addon_dir / "user_files" / "badges.json"
+pokenames_lang_path = addon_dir / "user_files" / "data_files" / "pokemon_species_names.csv"
+pokedesc_lang_path = addon_dir / "user_files" / "data_files" / "pokemon_species_flavor_text.csv"
+pokeapi_db_path = user_path_data / "pokeapi_db.json"
+starters_path = addon_dir / "addon_files" / "starters.json"
+eff_chart_html_path = addon_dir / "addon_files" / "eff_chart_html.html"
+effectiveness_chart_file_path = addon_dir / "addon_files" / "eff_chart.json"
+table_gen_id_html_path = addon_dir / "addon_files" / "table_gen_id.html"
+icon_path = addon_dir / "addon_files" / "pokeball.png"
+sound_list_path = addon_dir / "addon_files" / "sound_list.json"
+badges_list_path = addon_dir / "addon_files" / "badges.json"
+items_list_path = addon_dir / "addon_files" / "items.json"
+rate_path = addon_dir / "user_files" / "rate_this.json"
+csv_file_items = addon_dir / "user_files" / "data_files" / "item_names.csv"
+csv_file_descriptions = addon_dir / "user_files" / "data_files" / "item_flavor_text.csv"
+
+#effect sounds paths
+hurt_normal_sound_path = addon_dir / "addon_sprites" / "sounds" / "HurtNormal.mp3"
+hurt_noteff_sound_path = addon_dir / "addon_sprites" / "sounds" / "HurtNotEffective.mp3"
+hurt_supereff_sound_path = addon_dir / "addon_sprites" / "sounds" / "HurtSuper.mp3"
+ownhplow_sound_path = addon_dir / "addon_sprites" / "sounds" / "OwnHpLow.mp3"
+hpheal_sound_path = addon_dir / "addon_sprites" / "sounds" / "HpHeal.mp3"
+fainted_sound_path = addon_dir / "addon_sprites" / "sounds" / "Fainted.mp3"
+
+#pokemon species id files
+pokemon_species_normal_path = addon_dir / "user_files" / "pkmn_data" / "normal.json"
+pokemon_species_legendary_path = addon_dir / "user_files" / "pkmn_data" / "legendary.json"
+pokemon_species_ultra_path = addon_dir / "user_files" / "pkmn_data" / "ultra.json"
+pokemon_species_mythical_path = addon_dir / "user_files" / "pkmn_data" / "mythical.json"
+pokemon_species_baby_path = addon_dir / "user_files" / "pkmn_data" / "baby.json"
