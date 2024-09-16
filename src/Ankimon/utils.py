2eea62ece5b75d44b5bb420d7d93b364ee869b98
helpau <30323047+helpau@users.noreply.github.com>
2024-09-17 01:06:12 +0500

    Refactoring6 (#197)
    * read_github_file,compare_files,write_local_file
    
    * showWarning,tr,tooltip
    
    * HelpWindow
    
    * TableWidget(Effectiveness table)
    
    * IDTableWidget
    
    * read_html_file
    
    * Pokedex_Widget
    
    * bugfix
    
    * read_html_file call is commented
    
    * CheckFiles
    
    * unused markdown
    
    * CheckPokemonData
    
    * del CheckPokemonData
    
    * random_battle_scene
    
    * random_item, random_fossil
    
    * unused here
    
    ---------
    
    Co-authored-by: helpau <helpau@test.com>

--- src/Ankimon/utils.py
+++ src/Ankimon/utils.py
@@ -1,6 +1,10 @@
 import os
 import requests
 import json
+import markdown
+import random
+
+from .resources import battlescene_path, berries_path, items_path, itembag_path
 
 def check_folders_exist(parent_directory, folder):
     folder_path = os.path.join(parent_directory, folder)
@@ -59,3 +63,90 @@ def read_local_file(file_path):
             return file.read()
     except FileNotFoundError:
         return None
+    
+# Function to check if the file exists on GitHub and read its content
+def read_github_file(url):
+    response = requests.get(url)
+        
+    if response.status_code == 200:
+        # File exists, parse the Markdown content
+        content = response.text
+        html_content = markdown.markdown(content)
+        return content, html_content
+    else:
+        return None, None
+
+# Function to check if the content of the two files is the same
+def compare_files(local_content, github_content):
+    return local_content == github_content
+
+# Function to write content to a local file
+def write_local_file(file_path, content):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+def read_html_file(file_path):
+    """Reads an HTML file and returns its content as a string."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+def random_battle_scene():
+    # TODO: choice?
+    # TODO: merge with random_berries and
+    battle_scenes = {}
+    for index, filename in enumerate(os.listdir(battlescene_path)):
+        if filename.endswith(".png"):
+            battle_scenes[index + 1] = filename
+    # Get the corresponding file name
+    battlescene_file = battle_scenes.get(random.randint(1, len(battle_scenes)))
+    return battlescene_file
+
+def random_berries():
+    berries = {}
+    for index, filename in enumerate(os.listdir(berries_path)):
+        if filename.endswith(".png"):
+            berries[index + 1] = filename
+    # Get the corresponding file name
+    berries_file = berries.get(random.randint(1, len(berries)))
+    return berries_file
+
+def random_item():
+    # Initialize an empty list to store the file names
+    item_names = []
+    # Iterate over each file in the directory
+    for file in os.listdir(items_path):
+        # Check if the file is a .png file
+        if file.endswith(".png"):
+            # Append the file name without the .png extension to the list
+            item_names.append(file[:-4])
+    item_names = [name for name in item_names if not name.endswith("-ball")]
+    item_names = [name for name in item_names if not name.endswith("-repel")]
+    item_names = [name for name in item_names if not name.endswith("-incense")]
+    item_names = [name for name in item_names if not name.endswith("-fang")]
+    item_names = [name for name in item_names if not name.endswith("dust")]
+    item_names = [name for name in item_names if not name.endswith("-piece")]
+    item_names = [name for name in item_names if not name.endswith("-nugget")]
+    item_name = random.choice(item_names)
+    # add item to item list
+    with open(itembag_path, 'r') as json_file:
+        itembag_list = json.load(json_file)
+        itembag_list.append(item_name)
+    with open(itembag_path, 'w') as json_file:
+        json.dump(itembag_list, json_file)
+    return item_name
+
+def random_fossil():
+    fossil_names = []
+    # Iterate over each file in the directory
+    for file in os.listdir(items_path):
+        # Check if the file is a .png file
+        if file.endswith("-fossil.png"):
+            # Append the file name without the .png extension to the list
+            fossil_names.append(file[:-4])
+    fossil_name = random.choice(fossil_names)
+    with open(itembag_path, 'r') as json_file:
+        itembag_list = json.load(json_file)
+        itembag_list.append(fossil_name)
+    with open(itembag_path, 'w') as json_file:
+        json.dump(itembag_list, json_file, indent=2)
+    return fossil_name
