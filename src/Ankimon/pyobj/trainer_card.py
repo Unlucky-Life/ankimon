a509f84377a40fc5186986c0a48bacf7e5c53233
Unlucky-Life <77027147+Unlucky-Life@users.noreply.github.com>
2024-11-29 23:25:22 +0100

    adding pokedex, another button to open pokedex and xp gain function for trainer card aswell as generate random pokemon  new algorithm

--- src/Ankimon/pyobj/trainer_card.py
+++ src/Ankimon/pyobj/trainer_card.py
@@ -1,7 +1,22 @@
 from ..resources import trainer_sprites_path
 
+import math
+
+# Constants for leveling
+BASE_XP = 50  # Base XP required for level 1
+EXPONENTIAL_FACTOR = 1.5  # Scaling factor for exponential XP curve
+
+# Tier-based XP rewards (can be extended)
+POKEMON_TIERS = {
+    "normal": 10,
+    "baby": 30,
+    "ultra": 75,
+    "legendary": 500,
+    "mythical": 1000,
+}
+
 class TrainerCard:
-    def __init__(self, trainer_name, badge_count, favorite_pokemon, trainer_id, level=1, xp=0, achievements=None, team="", image_path=trainer_sprites_path, highest_level=0, league="unranked", cash="0"):
+    def __init__(self, trainer_name, badge_count, favorite_pokemon, trainer_id, logger, level=1, xp=0, achievements=None, team="", image_path=trainer_sprites_path, highest_level=0, league="unranked", cash="0"):
         self.trainer_name = trainer_name      # Name of the trainer
         self.badge_count = badge_count        # Number of badges the trainer has earned
         self.favorite_pokemon = favorite_pokemon  # Trainer's favorite Pokémon
@@ -28,17 +43,6 @@ class TrainerCard:
     def add_achievement(self, achievement):
         """Method to add a new achievement"""
         self.achievements.append(achievement)
-    
-    def gain_xp(self, points):
-        """Method to add experience points and level up if necessary"""
-        self.xp += points
-        self.level_up()
-
-    def level_up(self):
-        """Method to level up the trainer based on XP"""
-        while self.xp >= 100:
-            self.xp -= 100
-            self.level += 1
 
     def set_team(self, team_pokemons):
         """Method to set the trainer's active team (team as a string)"""
@@ -55,5 +59,27 @@ class TrainerCard:
             'favorite_pokemon': self.favorite_pokemon,
             'highest_level_pokemon': self.highest_level_pokemon,
             'team': self.team,
-            'achievements': self.achievements
+            'achievements': self.achievements,
+            'xp_for_next_level': self.xp_for_next_level
         }
+
+    def xp_for_next_level(self):
+        """Calculate XP required for the next level."""
+        return int(BASE_XP * math.pow(self.level, EXPONENTIAL_FACTOR))
+
+    def on_level_up(self):
+        """Triggered when leveling up."""
+        logger.log_and_showinfo("game", f"🎉 Congratulations! You reached Level {self.level}!")
+
+    def gain_xp(self, tier):
+        """Add XP based on defeated Pokémon's tier."""
+        xp_gained = POKEMON_TIERS.get(tier.lower(), 0)
+        self.xp += xp_gained
+        print(f"Gained {xp_gained} XP from defeating a {tier} Pokémon!")
+        self.check_level_up()
+
+    def check_level_up(self):
+        """Update level based on XP."""
+        while self.xp >= self.xp_for_next_level():
+            self.level += 1
+            self.on_level_up()
