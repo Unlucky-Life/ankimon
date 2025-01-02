47ec58ccb25c1ff6f384ea5065e87cbc4c0d657c
anon <anon@users.noreply.github.com>
2025-01-02 21:47:08 +0100

    added a preview image to choose a trainer sprite

--- src/Ankimon/gui_classes/choose_trainer_sprite.py
+++ src/Ankimon/gui_classes/choose_trainer_sprite.py
@@ -1,7 +1,10 @@
+from PyQt6.QtCore import Qt  # Add this import
 from PyQt6.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QComboBox, QPushButton
+from PyQt6.QtGui import QPixmap
 from aqt import mw
 from ..utils import get_all_sprites
 from ..resources import trainer_sprites_path
+import os
 
 class TrainerSpriteDialog(QDialog):
     def __init__(self, settings_obj, parent=mw):
@@ -9,6 +12,7 @@ class TrainerSpriteDialog(QDialog):
         self.setWindowTitle("Choose Your Trainer Sprite")
         self.settings = settings_obj
         self.trainer_sprites = get_all_sprites(trainer_sprites_path)
+        
         # Layout
         layout = QVBoxLayout()
 
@@ -16,11 +20,16 @@ class TrainerSpriteDialog(QDialog):
         label = QLabel("Choose your trainer sprite:")
         layout.addWidget(label)
 
+        # Image Preview (QLabel)
+        self.image_preview = QLabel()
+        layout.addWidget(self.image_preview)
+
         # Dropdown (ComboBox)
         self.dropdown = QComboBox()
         self.dropdown.addItems(self.trainer_sprites)
+        self.dropdown.currentTextChanged.connect(self.update_preview)
         layout.addWidget(self.dropdown)
-
+        
         # OK Button
         ok_button = QPushButton("OK")
         ok_button.clicked.connect(self.on_ok)
@@ -28,6 +37,19 @@ class TrainerSpriteDialog(QDialog):
 
         # Set layout
         self.setLayout(layout)
+
+        # Initial preview update
+        self.update_preview(self.dropdown.currentText())
+
+    def update_preview(self, selected_sprite):
+        # Construct the path to the sprite image
+        sprite_path = os.path.join(trainer_sprites_path, selected_sprite + ".png")
+        if os.path.exists(sprite_path):
+            pixmap = QPixmap(sprite_path)
+            self.image_preview.setPixmap(pixmap.scaled(150, 150))  # Resize the image to fit in the preview
+            self.image_preview.setAlignment(Qt.AlignmentFlag.AlignCenter)  # Center the image
+        else:
+            self.image_preview.clear()  # Clear the preview if no image found
 
     def on_ok(self):
         selected_sprite = self.dropdown.currentText()
