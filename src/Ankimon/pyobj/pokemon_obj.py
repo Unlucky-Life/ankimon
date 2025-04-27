20eb993d79dd1a20dc256fa7c132c103cde6c53c
anon <anon@users.noreply.github.com>
2025-04-26 19:09:35 -0600

    Added SirSkaro poke-engine to compute DAMAGE OUTPUT from each move

--- src/Ankimon/pyobj/pokemon_obj.py
+++ src/Ankimon/pyobj/pokemon_obj.py
@@ -4,9 +4,12 @@ from ..resources import pkmnimgfolder
 import os
 
 class PokemonObject:
-    def __init__(self, name="Ditto", shiny=False, id=1, level=3, ability=["None"], type=["Normal"], current_hp=15, stats=None, attacks=None, base_experience=0, 
-                 growth_rate=None, hp=None, ev=None, iv=None, gender=None, battle_status="Fighting", xp=0, 
-                 position=0, nickname=None, moves=None, evos=None, tier = "Normal", ev_yield = {"hp": 1, "atk": 0, "def": 0, "spa": 0, "spd": 0, "spe": 0}, friendship = 0, **kwargs):
+    def __init__(self, name="Ditto", shiny=False, id=1, level=3, ability=["None"], type=["Normal"], current_hp=15, stats=None, attacks=None, 
+                 base_experience=0, growth_rate=None, hp=None, ev=None, iv=None, gender=None, 
+                 battle_status="Fighting", xp=0, position=0, nickname=None, moves=None, evos=None, 
+                 tier="Normal", ev_yield=None, friendship=0, **kwargs):
+
+        self.item = kwargs.get('item', None)
         self.name = name
         self.nickname = nickname or "" # Allow nickname to be set in the constructor
         self.shiny = shiny or False  # Default to False if None
@@ -39,7 +42,18 @@ class PokemonObject:
         #friendship value
         self.friendship = friendship or 0
 
-        # Store battle stats for easy access
+        self.stat_stages = kwargs.get('stat_stages', {
+            'atk': 0,
+            'def': 0,
+            'spa': 0,
+            'spd': 0,
+            'spe': 0,
+            'accuracy': 0,
+            'evasion': 0
+        })
+        self.volatile_status = kwargs.get('volatile_status', set())
+        self.nature = kwargs.get('nature', 'serious')
+        self.item = kwargs.get('item', '')
         self._update_battle_stats()
     
     @classmethod
@@ -102,9 +116,91 @@ class PokemonObject:
                 return path
             else:
                 return default_path
+    
+    def to_engine_format(self):
+        from ..poke_engine.helpers import normalize_name
+        return {
+            'identifier': normalize_name(self.name),
+            'level': self.level,
+            'nature': getattr(self, 'nature', 'serious'),
+            'evs': (
+                self.ev.get('hp', 0),
+                self.ev.get('atk', 0),
+                self.ev.get('def', 0),
+                self.ev.get('spa', 0),
+                self.ev.get('spd', 0),
+                self.ev.get('spe', 0)
+            ),
+            'types': [normalize_name(t) for t in self.type],
+            'hp': self.current_hp,
+            'maxhp': self.max_hp,
+            'ability': normalize_name(self.ability[0]) if self.ability else 'none',
+            'item': None,
+            'attack': self.stats.get('atk', 0),
+            'defense': self.stats.get('def', 0),
+            'special_attack': self.stats.get('spa', 0),
+            'special_defense': self.stats.get('spd', 0),
+            'speed': self.stats.get('spe', 0),
+            'ivs': (
+                self.iv.get('hp', 0),
+                self.iv.get('atk', 0),
+                self.iv.get('def', 0),
+                self.iv.get('spa', 0),
+                self.iv.get('spd', 0),
+                self.iv.get('spe', 0)
+            ),
+            'attack_boost': self.stat_stages.get('atk', 0),
+            'defense_boost': self.stat_stages.get('def', 0),
+            'special_attack_boost': self.stat_stages.get('spa', 0),
+            'special_defense_boost': self.stat_stages.get('spd', 0),
+            'speed_boost': self.stat_stages.get('spe', 0),
+            'accuracy_boost': self.stat_stages.get('accuracy', 0),
+            'evasion_boost': self.stat_stages.get('evasion', 0),
+            'status': normalize_name(self.battle_status) if self.battle_status != "fighting" else None,
+            'volatile_status': set(normalize_name(vs) for vs in self.volatile_status),
+            'moves': [{'id': normalize_name(move)} for move in self.attacks]
+        }
+
+    @classmethod
+    def from_engine_format(cls, engine_data):
+        """Create PokemonObject from poke-engine data"""
+        return cls(
+            name=engine_data['identifier'].capitalize(),
+            level=engine_data['level'],
+            current_hp=engine_data['hp'],
+            stats={
+                'hp': engine_data.get('maxhp', 0),
+                'atk': engine_data['attack'],
+                'def': engine_data['defense'],
+                'spa': engine_data['special_attack'],
+                'spd': engine_data['special_defense'],
+                'spe': engine_data['speed']
+            },
+            ev={k: v for k, v in zip(['hp','atk','def','spa','spd','spe'], engine_data['evs'])},
+            iv={k: v for k, v in zip(['hp','atk','def','spa','spd','spe'], engine_data['ivs'])},
+            battlestatus=engine_data.get('status', 'fighting'),
+            moves=engine_data['moves'],
+            stat_stages={
+                'atk': engine_data['stat_stages']['attack'],
+                'def': engine_data['stat_stages']['defense'],
+                'spa': engine_data['stat_stages']['special_attack'],
+                'spd': engine_data['stat_stages']['special_defense'],
+                'spe': engine_data['stat_stages']['speed'],
+                'accuracy': engine_data['stat_stages']['accuracy'],
+                'evasion': engine_data['stat_stages']['evasion']
+            },
+            volatile_status=set(engine_data.get('volatile_status', [])),
+            nature=engine_data.get('nature', 'serious'),
+            item=engine_data.get('item', '')
+        )
 
 class PokemonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, PokemonObject):
-            return obj.__dict__  # Convert PokemonObject instance to dictionary
-        return json.JSONEncoder.default(self, obj)
+            data = obj.__dict__.copy()
+            # Convert complex types to serializable formats
+            data['volatile_status'] = list(data['volatile_status'])
+            data['stat_stages'] = data.get('stat_stages', {})
+            data['moves'] = data.get('attacks', [])
+            return data
+        return super().default(obj)
