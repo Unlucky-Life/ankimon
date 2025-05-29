54c3d9a17c76a66376d32ede051e90f2ecf0b362
Unlucky-Life <leon.moegelin@gmail.com>
2025-05-29 15:47:19 +0200

    Add functionality to forget Pokémon attacks and reroll daily shop items aswell as added new pokedex look aswell as ankimon tracker light and dark mode fix

--- src/Ankimon/pyobj/ankimon_shop.py
+++ src/Ankimon/pyobj/ankimon_shop.py
@@ -4,6 +4,8 @@ import random
 from datetime import datetime
 from ..utils import give_item, daily_item_list, get_item_price, get_item_description
 from ..resources import items_path
+
+
 # Daily Rotating Items Pool
 DAILY_ITEMS_POOL = daily_item_list()
 
@@ -22,6 +24,9 @@ class PokemonShopManager:
         self.settings_obj = settings_obj
         self.set_callback = set_callback
         self.get_callback = get_callback
+        self.number_of_daily_items = 3
+        self.daily_items_reroll_cost = 100
+        self.todays_daily_items = []
 
     def toggle_window(self):
         """Toggles the visibility of the Pokémon shop window."""
@@ -52,65 +57,74 @@ class PokemonShopManager:
 
         # --- Left Side for Daily Items ---
         daily_layout = QVBoxLayout()  # Vertical layout for daily items
+        daily_layout_title_and_button = QHBoxLayout()
         daily_label = QLabel("<h1>Daily Items</h1>")
         daily_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        daily_layout.addWidget(daily_label)
+        
+        # Reroll button to reroll daily items
+        daily_items_reroll_button = QPushButton(f"Reroll daily items  ${self.daily_items_reroll_cost}")
+        daily_items_reroll_button.setFixedSize(180, 25)
+        daily_items_reroll_button.clicked.connect(lambda: self.reroll_daily_items(cost=self.daily_items_reroll_cost))
+        daily_layout_title_and_button.addWidget(daily_items_reroll_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        daily_layout.addLayout(daily_layout_title_and_button, stretch=1)
 
         # Create a grid layout for displaying daily items
         daily_grid = QGridLayout()
 
-        daily_items = self.get_daily_items()
-        for row, item in enumerate(daily_items):
-            item['UI_NAME'] = item.get('name', 'poke-ball').replace('-', ' ').capitalize()
-            item['price'] = get_item_price(item.get('name', 'poke-ball'))
+        if not self.todays_daily_items:  # If the list of daily items is empty
+            self.todays_daily_items = self.get_daily_items()
+            for row, item in enumerate(self.todays_daily_items):
+                item['UI_NAME'] = item.get('name', 'poke-ball').replace('-', ' ').capitalize()
+                item['price'] = get_item_price(item.get('name', 'poke-ball'))
 
-            # Create a QFrame to wrap the row content
-            frame = QFrame()
-            frame.setStyleSheet("""
-                QFrame {
-                    border: 1px solid #FFFFFF;
-                    padding: 5px;
-                }
-            """)
+                # Create a QFrame to wrap the row content
+                frame = QFrame()
+                frame.setStyleSheet("""
+                    QFrame {
+                        border: 1px solid #FFFFFF;
+                        padding: 5px;
+                    }
+                """)
 
-            # Create a layout inside the frame for the row content
-            frame_layout = QHBoxLayout(frame)
+                # Create a layout inside the frame for the row content
+                frame_layout = QHBoxLayout(frame)
 
-            # Image
-            image_path = f"{items_path}/{item.get('name', 'poke-ball')}.png"
-            pixmap = QPixmap(image_path)
-            image_label = QLabel()
-            image_label.setPixmap(pixmap.scaled(50, 50, Qt.AspectRatioMode.KeepAspectRatio))
-            image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                # Image
+                image_path = f"{items_path}/{item.get('name', 'poke-ball')}.png"
+                pixmap = QPixmap(image_path)
+                image_label = QLabel()
+                image_label.setPixmap(pixmap.scaled(50, 50, Qt.AspectRatioMode.KeepAspectRatio))
+                image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-            # Name
-            name_label = QLabel(f"<h3>{item.get('UI_NAME', 'poke-ball')}</h3>")
-            name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                # Name
+                name_label = QLabel(f"<h3>{item.get('UI_NAME', 'poke-ball')}</h3>")
+                name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-            # Description
-            description_text = get_item_description(item['name'], self.settings_obj.get('misc.language', '9'))
-            description_label = QLabel(f"<em>{description_text}</em>")
-            description_label.setWordWrap(True)
-            description_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            description_label.setToolTip(description_text)
+                # Description
+                description_text = get_item_description(item['name'], self.settings_obj.get('misc.language', '9'))
+                description_label = QLabel(f"<em>{description_text}</em>")
+                description_label.setWordWrap(True)
+                description_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                description_label.setToolTip(description_text)
 
-            # Price
-            price_label = QLabel(f"<b>${item.get('price', 1000)}</b>")
-            price_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                # Price
+                price_label = QLabel(f"<b>${item.get('price', 1000)}</b>")
+                price_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-            # Buy button
-            buy_button = QPushButton("Buy")
-            buy_button.clicked.connect(lambda checked, item=item: self.buy_item(item))
+                # Buy button
+                buy_button = QPushButton("Buy")
+                buy_button.clicked.connect(lambda checked, item=item: self.buy_item(item))
 
-            # Add widgets to the frame layout
-            frame_layout.addWidget(image_label)  # Add Image label
-            frame_layout.addWidget(name_label)  # Add Name label
-            frame_layout.addWidget(description_label)  # Add Description label
-            frame_layout.addWidget(price_label)  # Add Price label
-            frame_layout.addWidget(buy_button)  # Add Buy button
+                # Add widgets to the frame layout
+                frame_layout.addWidget(image_label)  # Add Image label
+                frame_layout.addWidget(name_label)  # Add Name label
+                frame_layout.addWidget(description_label)  # Add Description label
+                frame_layout.addWidget(price_label)  # Add Price label
+                frame_layout.addWidget(buy_button)  # Add Buy button
 
-            # Add the frame to the grid layout
-            daily_grid.addWidget(frame, row, 0, 1, 4)
+                # Add the frame to the grid layout
+                daily_grid.addWidget(frame, row, 0, 1, 4)
 
         daily_layout.addLayout(daily_grid)
 
@@ -196,7 +210,7 @@ class PokemonShopManager:
         """Generate daily items based on the current date."""
         seed = datetime.now().strftime("%Y-%m-%d")
         random.seed(seed)
-        return random.sample(DAILY_ITEMS_POOL, 3)
+        return random.sample(DAILY_ITEMS_POOL, self.number_of_daily_items)
 
     def buy_item(self, item):
         try:
@@ -211,3 +225,31 @@ class PokemonShopManager:
         except Exception as e:
             self.logger.log_and_showinfo("error", f"Failed to purchase item: {e}")
             QMessageBox.critical(mw, "Purchase Failed", "An error occurred while purchasing the item.")
+
+    def reroll_daily_items(self, cost: int = 0) -> None:
+        # BUG : Closing and reopening Anki will reset the shop to this day's shop, not to the latest reroll
+        """
+        Rerolls the daily items in the shop
+
+        Args:
+            cost (int): The cost in Pokedollars to reroll the shop. Defaults to 0.
+
+        Returns:
+            None
+        """
+        # First, we check if the user actually has enough money to pay for the reroll
+        if self.settings_obj.get('trainer.cash', 0) < cost:
+            QMessageBox.critical(mw, "Purchase Failed", "You do not have enough money to reroll the shop.")
+            return
+        
+        # We substract the cost of the reroll to the amount of money the user has
+        self.set_callback('trainer.cash', int(self.get_callback('trainer.cash', 0) - cost))
+        self.currency_qlabel.setText(f"<h2>Current Cash: ${self.settings_obj.get('trainer.cash', 0)}</h2>")
+
+        # Rerolling the list of today's items
+        random.seed()
+        self.todays_daily_items = random.sample(DAILY_ITEMS_POOL, self.number_of_daily_items)
+
+        # Refreshing the window by closing and reopening it
+        self.toggle_window() # Closing the window
+        self.toggle_window() # Opening the window
