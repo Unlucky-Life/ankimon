69eb1bcd555784b1f227f5e54b74b0d7b62deef8
h0tp-ftw <noddy.manan@gmail.com>
2025-05-31 10:13:18 -0600

    Patch notes uses QTextBrowser instead of edit to make links clickable

--- src/Ankimon/gui_entities.py
+++ src/Ankimon/gui_entities.py
@@ -1,7 +1,7 @@
 import markdown
 import json
 from PyQt6.QtGui import QMovie, QIcon
-from PyQt6.QtWidgets import QLabel, QVBoxLayout, QTextEdit, QCheckBox, QPushButton, QMessageBox, QWidget, QScrollArea, QGridLayout
+from PyQt6.QtWidgets import QLabel, QVBoxLayout, QTextEdit, QCheckBox, QPushButton, QMessageBox, QWidget, QScrollArea, QGridLayout, QTextBrowser
 from aqt import mw
 from aqt.qt import QDialog, qconnect
 from aqt.utils import showWarning, showInfo, tooltip
@@ -87,21 +87,21 @@ class Version_Dialog(QDialog):
         self.setWindowTitle("Ankimon Notifications")
         self.setGeometry(100, 100, 600, 400)
         layout = QVBoxLayout()
-        self.text_edit = QTextEdit()
-        self.text_edit.setReadOnly(True)
-        self.text_edit.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.text_edit.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff) # For horizontal scrollbar, if you want it off
+        self.text_browser = QTextBrowser()
+        self.text_browser.setOpenExternalLinks(True)  # Enable clickable links
+        self.text_browser.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.text_browser.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.local_file_path = addon_dir / "update_notes.md"
         self.local_content = read_local_file(self.local_file_path)
         self.html_content = markdown.markdown(self.local_content)
-        self.text_edit.setHtml(self.html_content)
-        layout.addWidget(self.text_edit)
+        self.text_browser.setHtml(self.html_content)
+        layout.addWidget(self.text_browser)
         self.setWindowIcon(QIcon(str(icon_path)))
         self.setLayout(layout)
-    
+
     def open(self):
         self.exec()
-
+        
 class License(QWidget):
     def __init__(self):
         super().__init__()
