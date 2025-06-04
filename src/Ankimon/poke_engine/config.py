cebff0d6c4742476d7a426a1ea57efd7a57f2c82
anon <anon@users.noreply.github.com>
2025-06-04 09:25:58 -0600

    damage calculation considers ALL possibilities to make it more random

--- src/Ankimon/poke_engine/config.py
+++ src/Ankimon/poke_engine/config.py
@@ -2,7 +2,7 @@ class _ShowdownConfig:
     damage_calc_type: str
 
     def __init__(self):
-        self.damage_calc_type = 'average'
+        self.damage_calc_type = 'all'
 
 
 ShowdownConfig = _ShowdownConfig()
