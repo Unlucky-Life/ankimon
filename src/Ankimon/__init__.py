55f5a2d633032cdff8cf828090e202fbc9e81775
h0tp-ftw <141889580+h0tp-ftw@users.noreply.github.com>
2025-06-06 15:44:44 -0600

    Bring back morganite's lost commit

--- src/Ankimon/__init__.py
+++ src/Ankimon/__init__.py
@@ -4704,7 +4704,7 @@ addHook("profileLoaded", on_profile_loaded)
 
 def catch_shorcut_function():
     if enemy_pokemon.hp > 1:
-        tooltip("You only catch a pokemon once its fained !")
+        tooltip("You only catch a pokemon once it's fainted !")
     else:
         catch_pokemon("")
 
