25b7193c7343194a6cca00455efc771b5a495486
h0tp-ftw <141889580+h0tp-ftw@users.noreply.github.com>
2025-06-24 14:04:04 -0600

    Resources file now detects addon version and experimental or not

--- src/Ankimon/resources.py
+++ src/Ankimon/resources.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import json
 
 addon_dir = Path(__file__).parents[0]
 
@@ -8,6 +9,7 @@ user_path = addon_dir / "user_files"
 user_path_data = addon_dir / "user_files" / "data_files"
 user_path_sprites = addon_dir / "user_files" / "sprites"
 user_path_credentials = addon_dir / "user_files" / "data.json"
+manifest_path = addon_dir / "manifest.json"
 
 font_path = addon_dir / "addon_files"
 
@@ -98,6 +100,17 @@ backup_root = addon_dir / "user_files" / "backups"
 backup_folder_1 = backup_root / "backup_1"
 backup_folder_2 = backup_root / "backup_2"
 backup_folders = [os.path.join(backup_root, f"backup_{i}") for i in range(1, 4)]
+
+#detect add-on version
+try:
+    manifest = json.loads(manifest_path.read_text(encoding="utf-8"))
+    addon_ver = manifest.get("version", "unknown")
+except Exception:
+    addon_ver = "unknown"
+
+#note if it is an experimental build    
+IS_EXPERIMENTAL_BUILD = addon_ver.endswith("-E")
+
 
 POKEMON_TIERS = {
   "Normal": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 
