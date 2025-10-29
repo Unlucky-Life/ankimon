08a633aed00a26fec252ab36cafa1a261acef5c7
Kurt <kurt.bulau@yahoo.com>
2025-10-29 13:28:29 -0500

    fix bug with search_pokedex_by_id

--- src/Ankimon/functions/pokedex_functions.py
+++ src/Ankimon/functions/pokedex_functions.py
@@ -83,6 +83,8 @@ def search_pokedex_by_id(pokemon_id):
     with open(str(pokedex_path), "r", encoding="utf-8") as json_file:
             pokedex_data = json.load(json_file) 
             for entry_name, attributes in pokedex_data.items():
+                if "num" not in attributes:
+                    continue   #skip entries without num attribute in pokedex (most pokemon variants for new gens)
                 if attributes['num'] == pokemon_id:
                     return entry_name
     return 'Pokémon not found'
