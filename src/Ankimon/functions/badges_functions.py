b3b145a5f9f684b7031f99d36636e818809bf5f3
--global <--global@users.noreply.github.com>
2025-10-29 22:09:45 -0600

    feat: Ensure only one instance of achievements + minor improvements in badges_functions

--- src/Ankimon/functions/badges_functions.py
+++ src/Ankimon/functions/badges_functions.py
@@ -2,32 +2,30 @@ import json
 
 from ..resources import badgebag_path
 
-def check_badges(achievements):
-    with open(badgebag_path, "r", encoding="utf-8") as json_file:
-        badge_list = json.load(json_file)
-        for badge_num in badge_list:
-            achievements[str(badge_num)] = True
+def populate_achievements_from_badges(achievements):
+    # name change for clarification
+    try:
+        with open(badgebag_path, "r", encoding="utf-8") as json_file:
+            badge_list = json.load(json_file)
+            for badge_num in badge_list:
+                achievements[str(badge_num)] = True
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If file doesn't exist or is empty, just return the initial achievements
+        pass
     return achievements
 
 def check_for_badge(achievements, rec_badge_num):
-    achievements = check_badges(achievements)
-    if achievements[str(rec_badge_num)] is False:
-        got_badge = False
-    else:
-        got_badge = True
-    return got_badge
+    return achievements.get(str(rec_badge_num), False)
 
 def save_badges(badges_collection):
     with open(badgebag_path, 'w') as json_file:
         json.dump(badges_collection, json_file)
 
 def receive_badge(badge_num,achievements):
-    achievements = check_badges(achievements)
-    #for badges in badge_list:
     achievements[str(badge_num)] = True
     badges_collection = []
     for num in range(1,69):
-        if achievements[str(num)] is True:
+        if achievements.get(str(num)) is True:
             badges_collection.append(int(num))
     save_badges(badges_collection)
     return achievements
